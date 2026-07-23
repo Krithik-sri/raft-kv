0622bde8e847e7517e09ef06f271356a97e205ae
Krithik Srinivas <krithiksrinivas200@gmail.com>
2026-07-23 18:35:15 +0530

    feat: add RequestVote types for handling voting requests and responses

--- raft/types.go
+++ raft/types.go
@@ -14,3 +14,20 @@ type LogEntry struct {
 	Term    uint64
 	Command []byte
 }
+
+type Peer struct {
+	ID      NodeID
+	Address string
+}
+
+type RequestVoteRequest struct {
+	Term         uint64
+	CandidateID  NodeID
+	LastLogIndex uint64
+	LastLogTerm  uint64
+}
+
+type RequestVoteResponse struct {
+	Term        uint64
+	VoteGranted bool
+}
